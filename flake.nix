3e820295d542a3c11734336ac6b1c1e42697b3a7
municorn <municorn@musicaloft.com>
2026-03-04 13:42:23 -0700

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,109 @@
+{
+  description = "A Rust project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    musicaloft-style = {
+      url = "github:musicaloft/musicaloft-style";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rust-flake = {
+      url = "github:juspay/rust-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        inputs.rust-flake.flakeModules.default
+        inputs.rust-flake.flakeModules.nixpkgs
+
+        # sets up code formatting and commit linting
+        inputs.musicaloft-style.flakeModule
+      ];
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          ...
+        }:
+        let
+          pname = "rust-app";
+
+          buildInputs = [ ];
+          nativeBuildInputs = [ ];
+        in
+        {
+          # rust setup
+          devenv.shells.default = {
+            languages.rust = {
+              enable = true;
+              channel = "nightly";
+              mold.enable = true;
+            };
+
+            packages =
+              with pkgs;
+              [
+                bacon
+                cargo-outdated
+                cargo-tarpaulin
+              ]
+              ++ buildInputs
+              ++ nativeBuildInputs;
+
+            scripts.tarp.exec = ''cargo tarpaulin --engine llvm "$@"'';
+          };
+
+          # rust build settings
+          rust-project = {
+            # use the same rust toolchain from the dev shell for consistency
+            toolchain = config.devenv.shells.default.languages.rust.toolchainPackage;
+
+            # specify dependencies
+            defaults.perCrate.crane.args = {
+              inherit nativeBuildInputs buildInputs;
+            };
+          };
+
+          packages.default = config.rust-project.crates.${pname}.crane.outputs.packages.${pname};
+        };
+    };
+}
